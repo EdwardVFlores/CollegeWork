c5e87a8c70b352d2373c816bf4a53c71f246c3e5
Edward Flores <EdwardVFlores@gmail.com>
2019-09-16 13:43:30 -1000

    Updated with more values for class and groups

--- Physics2031/Lab1/StandardDeviation.java
+++ Physics2031/Lab1/StandardDeviation.java
@@ -2,8 +2,13 @@ package ProgrammingChallenges;
 
 public class StandardDeviation {
     public static void main(String[] args){
-        //int[] nums = new int[] {11,15,39,40,66,95,95,114,96,77,44,36,25}; //class values
-        int[] nums = new int[] {4,6,10,8,8,23,19,13,21,15,9,7,7}; // group values
+
+        int[] nums = new int[] {11,15,39,40,66,95,95,114,96,77,44,36,25}; // class values
+        //int[] nums = new int[] {1,	2,	5,	10,	18,	21,	16,	34,	20,	13,	5,	4,	2}; // group 1 values
+        //int[] nums = new int[] {1,	1,	7,	8,	18,	17,	21,	18,	14,	11,	18,	14,4}; // group 2 values
+        //int[] nums = new int[] {2,	4,	13,	5,	14,	11,	27,	24,	24,	15,	6,	3,	2}; // group 3 values
+        //int[] nums = new int[] {3,	2,	4,	9,	8,	23,	12,	25,	17,	23,	6,	8,	10}; // group 4 values
+        //int[] nums = new int[] {4,6,10,8,8,23,19,13,21,15,9,7,7}; // group 5 values
         double Variance = 0, numDarts = 0, sumOfNums = 0;
         // numDarts = number of darts thrown overall
         // sumOfNums = darts with points
@@ -13,7 +18,7 @@ public class StandardDeviation {
         }
         double firstNum = 1/Math.sqrt(numDarts);
         double mean = sumOfNums / numDarts;
-        System.out.println("VALUES FOR GROUP LAB");
+        System.out.println("VALUES FOR GROUP 5");
         System.out.printf("Mean: %.2f\n",mean);
         for(int i = 1; i < nums.length + 1; i++) Variance += Math.pow((i - mean), 2) * nums[i - 1];
         double StandardDev = firstNum * Math.sqrt(Variance);
@@ -29,7 +34,6 @@ public class StandardDeviation {
         for(int i = 1; i < nums.length + 1; i++) {
             //if(i%2==1) {
                 double secondPart = Math.exp((-Math.pow((i - mean), 2)) / (2 * Math.pow(StandardDev, 2)));
-                //System.out.println(firstPart*secondPart);
                 dartsAtLocation[i - 1] = firstPart * secondPart;
                 System.out.printf("Bin #%d: %.2f\n", i, firstPart * secondPart);
             //}
